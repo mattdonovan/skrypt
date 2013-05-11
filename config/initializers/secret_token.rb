789677918489195a3299f09e2ab9e198f42f408b
Matt Donovan <iam.mattdonovan@gmail.com>
2013-05-10 23:44:01 -0700

    Getting the rails environment set up for a screenwriting app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skrypt::Application.config.secret_token = '089f12bac3a69305925786852e93f0486a8e1fb291fafa9acd4f4ea75319cc5d4640b8f9664595dbb50cc2b74c04c4711a6eb72c27ab434f46004134d956dd94'
